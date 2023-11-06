9f4129b40c5622d8fcd1ef22c5d38656ce6d2864
Idooru <shere1765@gmail.com>
2023-11-06 19:20:05 +0900

    함수 호출 자동화

--- src/resources/js/event/init/page-init.mjs
+++ src/resources/js/event/init/page-init.mjs
@@ -1,9 +1,10 @@
 import { buttonEventInit } from "./button-event-init.mjs";
 import { keyupEventInit } from "./keyup-event-init.mjs";
+import { focusEventInit } from "./focus-event-init.mjs";
 
 const pageInit = () => {
-  buttonEventInit();
-  keyupEventInit();
+  const inits = [buttonEventInit, keyupEventInit, focusEventInit];
+  inits.forEach((init) => init());
 };
 
 window.addEventListener("load", pageInit);
