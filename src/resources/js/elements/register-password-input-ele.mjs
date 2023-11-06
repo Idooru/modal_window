990db3fe346dfdae194d1fd0d99748d903a4fbf9
Idooru <shere1765@gmail.com>
2023-11-06 17:11:56 +0900

    회원가입 비밀번호 입력값 html 요소

---
+++ src/resources/js/elements/register-password-input-ele.mjs
@@ -0,0 +1,2 @@
+export const registerPasswordInputEle = document.querySelector("#register_password");
+export const registerPasswordConfirmInputEle = document.querySelector("#register_password_confirm");
